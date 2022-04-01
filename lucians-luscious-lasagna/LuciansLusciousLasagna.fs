34cd4269214456b90f61ffca4c66f2b2a2c5975d
Jesse Junior <jesse@4d.bsb.br>
2022-04-01 09:15:04 -0300

    first commit

---
+++ lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -0,0 +1,16 @@
+module LuciansLusciousLasagna
+
+
+// TODO: define the 'expectedMinutesInOven' binding
+
+let expectedMinutesInOven = 40
+
+// TODO: define the 'remainingMinutesInOven' function
+let remainingMinutesInOven elapsed = expectedMinutesInOven - elapsed
+
+// TODO: define the 'preparationTimeInMinutes' function
+
+let preparationTimeInMinutes nrOfLayers = nrOfLayers * 2
+
+// TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes a b = a * 2 |> (+) b
