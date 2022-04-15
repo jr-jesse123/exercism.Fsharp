6f5ae45f09d335d74c952799d6fd8f76524d3bed
Jesse Junior <jesse@4d.bsb.br>
2022-04-15 15:36:21 -0300

    my solution

--- bank-account/BankAccount.fs
+++ bank-account/BankAccount.fs
@@ -9,46 +9,63 @@ type BalanceMsg =
 | GetBalance of AsyncReplyChannel<Decimal>
 | ChangeBalance of Decimal
 
-type AccountInfo  = 
-    {
-        Id:Guid 
-        mutable History: Decimal List
-        mutable State: State
-    }
-// with 
-//     val  private  teste:int
-//     member this.MyReadWriteProperty
-//         with get () = this.teste
-//         and set (value) = this.teste <- value
-        
-    
-module AccountInfo = 
-    let Balance this = this.History |> List.sum
-    let Create () =  {Id=Guid.NewGuid() ; History = [] ; State=Closed}
-    let UpdateBalance value this = 
-        this.History <- value::this.History
+type AccountInfo ()  = 
+    let mutable History : decimal List = []
+    let mutable state = Closed
+    let getBalance() = History |> List.sum
+    let updateBalance value  =  History <- value::History
+
+    let balanceAgent = MailboxProcessor.Start(fun inbox ->
+        let rec innerLoop() = async{
+            match! inbox.Receive() with
+            | GetBalance repplyChannel -> repplyChannel.Reply (getBalance())
+            | ChangeBalance value -> updateBalance value
+
+            return! innerLoop()
+        }
+        innerLoop()
+
+    )
+
+    member _.Id = Guid.NewGuid()
+    member _.Balance ()  = 
+        balanceAgent.PostAndReply GetBalance
+
+    static member Create () =  AccountInfo()
+
+    member this.UpdateBalance value  = 
+        balanceAgent.Post (ChangeBalance value)
         this
 
-    let Open this = {this with State= Open}
-    let Close this = {this with State = Closed}
+    member this.Open = 
+        state<- Open
+        this
+    
+    member this.Close  = 
+        state <- Closed
+        this
+
+    member _.State = state
+
+    
 
 let mkBankAccount() = AccountInfo.Create()
 
-let openAccount account = 
+let openAccount (account: AccountInfo) = 
     match account.State with
-    | Closed -> AccountInfo.Open account
+    | Closed ->  account.Open
     | _ -> failwith "can´t open already opened account"
 
-let closeAccount account = 
-    AccountInfo.Close account
+let closeAccount (account: AccountInfo) = 
+    account.Close
 
-let getBalance account = 
+let getBalance (account: AccountInfo) = 
     match account.State with
-    | Open  -> Some (AccountInfo.Balance account)
+    | Open  -> Some (account.Balance ())
     | Closed -> None
 
 
-let updateBalance change account = 
+let updateBalance change (account: AccountInfo) = 
     match account.State with
-    | Open  ->  (AccountInfo.UpdateBalance change account)
+    | Open  ->  (account.UpdateBalance  change)
     | Closed -> failwith ""
