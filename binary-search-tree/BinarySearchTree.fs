cb3f61af93643ed14874e2a98b3e928b46a90868
Jesse Junior <jesse@4d.bsb.br>
2022-04-16 09:05:26 -0300

    binary tree better solution

--- binary-search-tree/BinarySearchTree.fs
+++ binary-search-tree/BinarySearchTree.fs
@@ -1,66 +1,39 @@
 module BinarySearchTree
 
-type BST = {
-    value:int
-    mutable left:BST option
-    mutable right:BST option
-}
-module BST = 
-    let fromValue v = {value=v;left=None;right=None}
+type Tree =
+| Empty 
+| Node of value:int * left:Tree * right:Tree
 
-let left node  = 
-    node.left
+[<AutoOpen>]
+module Tree = 
+    let fromValue value = Node (value,Empty,Empty)
+    let right node =
+        match node with
+        | Empty | Node (_,_,Empty) -> None
+        | Node (_,_,r) -> Some r
 
-let right node = 
-    node.right
+    let left node =
+        match node with
+        | Empty | Node (_,Empty,_) -> None
+        | Node (_,l,_) -> Some l
 
-let data node = 
-    node.value
+    let data = function
+    | Empty -> failwith ""
+    | Node (v,l,r) -> v
+    
 
-type leftRight =
-|Left 
-|Right
-
-let toLeft_Right vbase vnode = 
-    if vnode <= vbase then Left else Right
-
-let rec FindNodeWithEmptySlot nodeBase value =
-    match toLeft_Right nodeBase.value value with
-    | Left when nodeBase.left.IsNone -> nodeBase , Left
-    | Left when nodeBase.left.IsSome -> FindNodeWithEmptySlot nodeBase.left.Value value
-    | Right when nodeBase.right.IsNone -> nodeBase , Right
-    | Right  when nodeBase.right.IsSome -> FindNodeWithEmptySlot nodeBase.right.Value value
-    | _ -> failwith ""
-
+let create items =
+    let rec insert item node = 
+        match node with
+        | Empty -> Tree.fromValue item
+        | Node (v,l,r) when item > v -> Node (v,l,insert item r)
+        | Node (v,l,r) -> Node (v,insert item l, r)
+    List.fold (fun tree item -> insert item tree) Empty items
+    
 
 
-let  create (items: int list) =
-    let baseNode = BST.fromValue items[0]
- 
-    for item in items[1..] do
-        let suitableNode = FindNodeWithEmptySlot baseNode item
-        match suitableNode with
-        |node, Left ->  node.left <- BST.fromValue item |> Some
-        |node, Right -> node.right <- BST.fromValue item |> Some
-
-    baseNode
-
-let rec toList node = 
-    [
-        yield!
-            match node.left with
-            |Some leftNode-> toList leftNode
-            |None ->  []
-        
-        node.value 
-        
-        yield!
-            match node.right with
-            |Some righNode-> toList righNode
-            |None ->  []
-        
-    ]
-
-let sortedData node = 
-    toList node |> List.sort
+let rec sortedData node =
+    match node with
+    | Empty -> []
+    | Node (v,l,r) -> sortedData l @ [v] @ sortedData r
 
