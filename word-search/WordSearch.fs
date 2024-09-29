e59da701ef6bc1f181c50750b2f6618d1edff52a
Jesse Junior <jejunior@support.connectionsAcademy.org>
2024-09-29 11:16:45 -0300

    funcionando sem mapeamento

--- word-search/WordSearch.fs
+++ word-search/WordSearch.fs
@@ -9,18 +9,49 @@ type OptBuilder() =
 
 let optBuilder = OptBuilder()
 
+type charWithPos = 
+    {
+        char: char
+        pos: int * int
+    }
+
+
+
+
 let findWordLeftToRight (grid: string list) (word: string) =
-    grid
-    |> List.mapi(fun idx line-> {| y= idx + 1; x=line.IndexOf word + 1|})
-    |> List.tryFind (fun tuple -> tuple.x > 0)
-    |> Option.map (fun coords -> 
-        (coords.x, coords.y), (coords.x + word.Length - 1 , coords.y))
+    let originalPositions = 
+        grid
+        |> List.mapi(fun ydx line-> 
+            line.ToCharArray() |> Array.mapi (fun xidx c -> {char=c;pos = xidx, ydx})
+            )
+    
+    originalPositions
+    |> List.map (Array.map (fun x -> x.char)  ) 
+    |> List.map String
+    |> List.mapi (fun y line -> (line.IndexOf word ) , y )
+    |> List.tryFind (fun (x, y) -> x >= 0)
+    |> Option.map (fun (x,y) -> 
+        let foundPositons = 
+            [
+                for pos in x..(x + word.Length - 1) do
+                    yield originalPositions[y][pos]
+            ]       
+        foundPositons.Head.pos , foundPositons |> List.last |> _.pos
+        )
+    |> Option.map (fun ((xa,ya), (xb,yb)) ->
+        ((xa + 1 ,ya + 1), (xb + 1,yb + 1)) 
+    )
+
+    //|> List.tryFind (fun tuple -> tuple.x > 0)
+    //|> Option.map (fun coords -> 
+    //    (coords.x, coords.y), (coords.x + word.Length - 1 , coords.y))
 
 let findWordRightToLeft grid (word: string)= 
     let invertXasis ((xa, ya),(xb,yb))  =  (xb,ya) , (xa,yb)
 
     word.ToCharArray() 
-    |> (Array.rev >> String) 
+    |> (Array.rev >> String)
+    //|> (Array.rev >> String) 
     |> findWordLeftToRight grid 
     |> Option.map invertXasis
 
@@ -67,15 +98,6 @@ let findTopToBotton grid word =
 
 
 
-
-//findWordLeftToRight (Grid.ToLeftTopBottonRight (Array.ofList grid)) "java"
-//|> Option.map(fun ((xa, ya), (xb,yb)) -> 
-//   let xStream = [xa..xb]
-//   let yStream = [ya..yb]
-//   List.allPairs xStream yStream
-
-//)
-
 let findBottonToTop grid word = 
     let columGrid = 
         grid
@@ -90,40 +112,57 @@ let findBottonToTop grid word =
 
 
 
-let findTopLeftToBottonRight grid word = 
-    let columGrid = 
-        grid
-        |> List.toArray
-        |> Grid.ToLeftTopBottonRight
-        //|> List.rev // fixes the y idx
+let findTopLeftToBottonRight (grid: string list) (word: string) = 
+    let edges = 
+            [for a in 0..9 do yield a, 0] @
+            [for a in 0..9 do yield 0, a] 
+            |> List.distinct
         
 
-    let invertXYasis ((xa, ya),(xb,yb))  =  
-        let highgt = columGrid.Length
-        let width = columGrid[0].Length
 
-        let middle = (highgt + 1) / 2
+    let ToLeftTopBottonRight (grid: 'a array array) = 
+            [
+                for edge in edges do 
+                    
+                        yield 
+                            [|
+                                let mutable (x, y ) = edge
+                                
+                                while y < grid.Length && x < grid[0].Length do
+                                    yield grid[y][x]
+                                    x  <- x + 1 
+                                    y <- y + 1
+                            |]
+               
+                //for offset in 0..(grid.Length - 1) do 
+                //     yield   
+                //        String.replicate (grid.Length - offset - 1) " " + 
+                //        grid[offset]  + 
+                //        String.replicate offset " "
+            ]
+            |> List.toArray
 
-        let fixX y x = 
-            match y - middle with
-            | offsetBottom when offsetBottom > 0 -> x - offsetBottom
-            | offSetTop when offSetTop < 0 -> x + offSetTop
-            | middle -> x
 
-        let fixY x y = 
-            match y - middle with
-            | offsetTop when offsetTop > 0 -> 
-                y + offsetTop + x - 1
-            | offSetBotton -> 
-                y - middle + x 
-            //| middle -> x + y - 1
-            //y - 9
-
-        (fixX ya xa, fixY xa ya) , (fixX yb xb, fixY xb yb)
-
-    findWordLeftToRight columGrid word
-    |> Option.map invertXYasis    
-
+    let originalPositions = 
+        grid
+        |> List.mapi(fun ydx line-> 
+            line.ToCharArray() |> Array.mapi (fun xidx c -> {char=c;pos = xidx, ydx})
+            )
+    
+    grid
+    |> List.toArray
+    |> Array.map (_.ToCharArray())
+    |> ToLeftTopBottonRight
+    |> Array.map String
+    |> Array.mapi (fun y line -> (line.IndexOf word ) , y )
+    |> Array.tryFind (fun (x, y) -> x >= 0)
+    |> Option.map (fun (x,y) -> (x,y) , (x + word.Length - 1,y))
+    |> Option.map (fun ((xa,ya), (xb,yb)) ->
+        ((xa ,ya ), (xb ,yb + xb )) 
+    )
+    |> Option.map (fun ((xa,ya), (xb,yb)) ->
+        ((xa + 1 ,ya + 1), (xb + 1,yb + 1)) 
+    )
 
 let  findTopBottonRightToTopLeft grid word =
     let columGrid = 
@@ -133,7 +172,6 @@ let  findTopBottonRightToTopLeft grid word =
         |> List.map Seq.rev
         |> List.map Array.ofSeq
         |> List.map String
-        //|> List.rev // fixes the y idx
         
 
     let invertXYasis ((xa, ya),(xb,yb))  =  
@@ -177,6 +215,7 @@ let  findTopBottonRightToTopLeft grid word =
 
     findWordLeftToRight columGrid word
     |> Option.map invertXYasis        
+
 
 let findWords grid word = 
     [
