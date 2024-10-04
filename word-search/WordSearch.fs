14fa45f722bb50e000bd45ebfe69520d9749e91a
Jesse Junior <jejunior@support.connectionsAcademy.org>
2024-10-04 15:05:17 -0300

    todos testes passando

--- word-search/WordSearch.fs
+++ word-search/WordSearch.fs
@@ -2,6 +2,7 @@ module WordSearch
 
 open System.Reflection
 open System
+open System.Runtime.InteropServices
 
 (*
     specs:
@@ -32,9 +33,9 @@ type Position = {X:XPosition;Y:YPosition}
 type PositionedChar = {Char:char;Position:Position}
 type SpanPosition = {Start:Position; End:Position}
 
-type PositionedString = {Str: string; SpanPosition: SpanPosition }
+type PositionedString = {Str: string; SpanPosition: SpanPosition option }
 
-type CharGrid = PositionedChar list list 
+type CharGrid = PositionedChar list 
 type WordPosition = Position * Position
 
 
@@ -44,132 +45,119 @@ type GetWordsInAllDirections = CharGrid -> CharGrid list
 
 type MatchTargetWords = CharGrid -> string list -> Map<string, WordPosition option>
 
-type GetStrings = CharGrid -> string list -> (PositionedString) list
+type GetStrings = string list -> CharGrid -> PositionedString list
 
 type SortGrid = CharGrid -> CharGrid
 
 type ToString = PositionedChar list -> string
 //use case
-type Search = CharGrid -> string list -> Map<string, (int * int) * (int * int) option>
+type Search = CharGrid -> string list -> Map<string, ((int * int) * (int * int)) option>
 
 //implementation
 let toPositionedChars : ToPositionedChars = 
-    fun (input) ->
+    fun (input: string list) ->
         input 
         |> List.mapi (fun yIdx line -> 
             line.ToCharArray() 
             |> Array.mapi (fun xIdx char ->  { Char=char; Position= {X= XPosition xIdx; Y= YPosition yIdx} } )
-            |> Array.toList
         )
-
-//let CharGrid = toPositionedChars grid
+        |> Array.concat
+        |> Array.toList
 
 let makeTopDownGrid : SortGrid =
-    fun grid ->
-        List.concat grid
-        |> List.sortBy(fun char -> char.Position.Y) 
-        |> List.sortBy (fun char -> char.Position.X)   
-        |> List.groupBy (fun char -> char.Position.Y) 
-        |> List.map snd
+         List.sortBy(fun char -> char.Position.Y) 
+        >> List.sortBy (fun char -> char.Position.X)   
+       
+let makeTopLeftBottonDownGrid : SortGrid= 
+    List.sortBy (fun c -> c.Position.X.value - c.Position.Y.value)
         
-
-
-let makeTopLeftBottonDownGrid = 
-    //let isSameLine x y = 
+let makeBBottonLeftTopUpGrid : SortGrid = 
+      List.sortBy(fun char -> (-char.Position.Y.value * 1000) + (-char.Position.X.value * 1000)) 
         
-    fun grid ->
-        List.concat grid
-        |> List.sortBy(fun char -> char.Position.Y.value + char.Position.X.value) 
-        |> List.sortBy (fun char -> char.Position.X)   
-        |> List.fold (fun state currChar ->  //this should not bbe a list
-            let previousLineOpt = state |> List.tryLast
-            match previousLineOpt with
-            | None -> [[currChar]]
-            | Some (previousLine)-> 
-                if (List.last previousLine |> _.Position.X) >  currChar.Position.X then
-                    
-                        (previousLine @ [ currChar ]) :: state[..state.Length - 2]
-                        |> List.rev
-                        
-                else
-                    [ currChar ] :: state
-                    |> List.rev
-                   
-
-            //state
-            ) []
-        //|> List.map snd
-        
-    
-let makeBBottonLeftTopUpGrid = 
-    
-     fun grid ->
-         List.concat grid
-         |> List.sortBy(fun char -> char.Position.Y.value + char.Position.X.value) 
-         |> List.sortBy (fun char -> char.Position.X)   
-         |> List.fold (fun state currChar ->  //this should not bbe a list
-             let previousLineOpt = state |> List.tryLast
-             match previousLineOpt with
-             | None -> [[currChar]]
-             | Some (previousLine)-> 
-                 if (List.last previousLine |> _.Position.X) <  currChar.Position.X then
-                     
-                         (previousLine @ [ currChar ]) :: state[..state.Length - 2]
-                         |> List.rev
-                         
-                 else
-                     [ currChar ] :: state
-                     |> List.rev
-                    
-
-             //state
-             ) []
-
 let toString: ToString = 
     fun charList ->
         charList |> List.map _.Char |> List.toArray |> String
 
-let getString  : GetStrings =
-    fun (grid: CharGrid) (words: string list) ->
-       grid
-       |> List.map (fun line ->
-            let text = toString line
-            words |> List.map (fun word -> 
-                let idx = text.IndexOf word 
-                if idx > -1 then 
-                    let pos = {Start=line[idx].Position;End=line[idx + word.Length - 1].Position}
-                    Some {Str=word; SpanPosition = pos} 
-                else
-                    None
-                )
-       )
-       |> List.map (fun x -> x |> List.choose id)
-       |> List.concat
-       //[]
+let getStrings  : GetStrings =
+    let isContinuousChar previousChar currChar =
+        let previousPosition = previousChar.Position
+        let currentPosition = currChar.Position
+        let isSameLine = previousPosition.Y = currentPosition.Y
+        let isSameColumn = previousPosition.X.value = currentPosition.X.value 
+        let isDiagonal = 1 = abs (previousPosition.Y.value - currentPosition.Y.value ) && 1 = abs (previousPosition.X.value - currentPosition.X.value)
+        isSameLine || isSameColumn || isDiagonal
+        
+    let folder acc currChar =
+        match acc with
+        |[] -> [[currChar]]
+        | acc ->
+            let lastLine = List.last acc
+            if isContinuousChar (List.last lastLine) currChar then
+                acc[..acc.Length - 2] @ [lastLine @ [currChar]]
+            else
+                acc @ [[currChar]]
 
+    let getPositionedStrings  (words: string list) line =
+        let text = toString line
+        words 
+        |> List.map (fun word -> 
+            let idx =  text.IndexOf word
+            if idx > -1 then 
+                let pos = {Start= line[idx].Position; End=line[idx + word.Length - 1].Position}
+                {Str=word;SpanPosition=Some pos}
+            else
+                {Str=word;SpanPosition=None}
+        )
+
+    fun words grid ->
+        let getPositionedStrings'= getPositionedStrings words
+        let x= 
+            grid
+            |> List.fold folder []
+            |> List.map getPositionedStrings'
+        
+        List.concat x
+        
 let getWordsInAllDirections : GetWordsInAllDirections =
     fun originalGrid ->
         [
             id // left to right
-            //List.rev //right to left
+            List.rev //right to left
             makeTopDownGrid
+            makeTopDownGrid >> List.rev
+            makeTopLeftBottonDownGrid
+            makeTopLeftBottonDownGrid >> List.rev
             makeBBottonLeftTopUpGrid 
-            
+            makeBBottonLeftTopUpGrid >> List.rev
         ]
         |> List.map (fun func -> func originalGrid)
         
 
 let search' : Search = 
-    fun grid searchedWords ->
-        let allGrids = getWordsInAllDirections grid
+    let spanToDto (spanPos: SpanPosition ) =
+        spanPos
+        |>  (fun {Start=start;End=``end``} -> 
+            (start.X.value + 1, start.Y.value + 1), (``end``.X.value + 1, ``end``.Y.value + 1 )
+        )
 
-
-        Map.empty
-    
-
-
+    fun grid wordsToSearchFor ->
+        let getPositionedStrings = getStrings wordsToSearchFor 
+        getWordsInAllDirections grid
+        |> List.map getPositionedStrings
+        |> List.concat
+        |> List.map (fun positionedStr -> 
+            positionedStr.Str, positionedStr.SpanPosition |> Option.map spanToDto 
+        )
+        |> List.groupBy fst
+        |> List.map (fun (key, group) -> List.sortBy (snd >> Option.isSome) (group))
+        
+        |> List.concat
+        |> Map.ofList
+        
 
 let search grid wordsToSearchFor: Map<string,((int * int) * (int * int)) option>    =
-    List.map (fun x -> x, None) wordsToSearchFor
-    |> Map.ofList
+    search' (toPositionedChars grid) wordsToSearchFor
+
+
+
 
